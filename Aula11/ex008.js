9a27d149874ab175f0c7d0667c8b2e76f4d86399
Romulo Damazio <rvdamazio@gmail.com>
2023-04-28 09:24:30 -0300

    condições if e else

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+
+var vel= 90
+console.log(`A velocidade do seu carro é ${vel} km/h!`)
+if (vel > 60){ //CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassaou a velocidade permitida, você foi, MULTADO!`)
+}
+console.log(`Dirija usando sempre o cinto de segurança`)
