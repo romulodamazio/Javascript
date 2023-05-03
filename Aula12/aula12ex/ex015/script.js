d5b0f5470799d10b478001d43bd5818f3bf6d379
Romulo Damazio <rvdamazio@gmail.com>
2023-05-03 14:33:04 -0300

    subinova

--- Aula12/aula12ex/ex015/script.js
+++ Aula12/aula12ex/ex015/script.js
@@ -10,7 +10,46 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+
+        if (fsex[0].checked) {
+            gênero ='Homem'
+
+            if (idade >=0 && idade < 10){
+                //criança
+                img.setAttribute (' src' , 'foto-crianca-m.png')
+            }
+            else if ( idade < 21){
+                //jovem
+            }
+            else if ( idade < 50){
+                //adulto 
+            }
+            else {
+                //idoso 
+            }
+        } 
+        
+        else if (fsex[1].checked) {
+            gênero ='Mulher'
+            if (idade >=0 && idade < 10){
+                //criança
+            }
+            else if ( idade < 21){
+                //jovem
+            }
+            else if ( idade < 50){
+                //adulto 
+            }
+            else {
+                //idoso 
+            }
+        }
+        res.style.textAlign ='center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        
 
     }
 }
